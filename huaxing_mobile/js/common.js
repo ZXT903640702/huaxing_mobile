652e2a209e1828d180e6235a46979c2ea5e6edbc
Qjn <123456789@qq.com>
2019-04-17 17:14:34 +0800

    完成的页面

---
+++ huaxing_mobile/js/common.js
@@ -0,0 +1,28 @@
+$("html").css("fontSize",$(window).width() * 0.13333333333333333);
+$(window).on("resize",function(){
+    $("html").css("fontSize",$(window).width() * 0.13333333333333333);
+});
+
+// 导航条控制
+$(".pageNav .firstNav li a").on("click",function(){
+    if($(this).hasClass('action')){
+        $(this).removeClass('action');
+        return;
+    };
+    $(this).parent().siblings().find("a").removeClass("action");
+    $(this).addClass("action");
+});
+$(".pageNav .header .navBtn").on("click",function(){
+    if($(this).hasClass('current')){
+        $(this).removeClass('current');
+        $(".pageNav .firstNav").removeClass("current");
+    return;
+    };
+    $(this).addClass("current");
+    $(".pageNav .firstNav").addClass("current");
+});
+
+//回到顶部
+$(".scrollTop").on("click",function(){
+    $("html").stop().animate({"scrollTop":0},500);
+});
